dfedc13df8263d2327192bafca74dee27890cc5d
DESKTOP-QJD5U69\George <gmihalios@yahoo.com>
2019-10-10 16:26:34 +0300

    -- Added gulp watcher to compile .scss files. -- Added all images to html. -- Added some basic styling/layout.

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const { src, dest, parallel, watch } = require('gulp');
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-csso');
+const path = require('path');
+
+const paths = {
+  scss: 'styles'
+};
+
+const css = () => {
+  return src(`${paths.scss}/*.scss`)
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(dest(__dirname));
+};
+
+exports.css = css;
+
+exports.default = cb => {
+  css();
+  cb();
+};
+
+exports.watch = cb => {
+  watch([`styles/*.scss`], function(cb) {
+    css();
+    cb();
+  });
+};
